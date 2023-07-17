3d93830b28944cf51672845643dacf93a93547cd
hma14 <henryma14@gmail.com>
2023-07-17 02:00:43 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template
+from server.chat import start_chat
+import jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+
+@app.route('/chat', methods=['POST'])
+def chat():
+  content = request.form.get('text')
+  chat_reply = start_chat(content)
+  return chat_reply
+
+
+if __name__ == '__main__':
+  #app.run()
+  app.run(host='0.0.0.0', port=5000, debug=True)
